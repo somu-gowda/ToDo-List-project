6eba6f7c662db41226230577be3b63a58741f7f7
somu-gowda <srbsomu123@gmail.com>
2021-07-13 21:54:12 +0530

    first commit

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./NewExpense.css"
+import ExpenseForm from './ExpenseForm';
+const NewExpense = () => {
+    return (
+<div   className="new-expense">
+    <span className= "font"> test </span>
+      <ExpenseForm />
+    
+</div>
+
+
+    );
+
+}
+
+export default NewExpense;
